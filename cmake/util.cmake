435c75bd569371638b7dc7cfe2ee48d59167eca9
JokAlex <JokAlex2005@yandex.ru>
2025-12-12 12:13:25 +0300

    Replaced custom installing and packaging with the ones from Borsch
    ILB-110

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -20,9 +20,10 @@
 # along with this script.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-function(check_version major minor rev)
+include(${CMAKE_CURRENT_LIST_DIR}/borsch/cmake/version.cmake)
 
-    # parse the version number from expat.h and include in
+function(check_version major minor rev)
+    # parse the version number from gif_lib.h and include in
     # major, minor and rev parameters
     set(VERSION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/gif_lib.h)
 
@@ -45,102 +46,5 @@ function(check_version major minor rev)
     set(${minor} ${_MINOR_VERSION} PARENT_SCOPE)
     set(${rev} ${_PATCH_VERSION} PARENT_SCOPE)
 
-    # Store version string in file for installer needs
-    file(TIMESTAMP ${VERSION_FILE} VERSION_DATETIME "%Y-%m-%d %H:%M:%S" UTC)
-    set(VERSION ${_MAJOR_VERSION}.${_MINOR_VERSION}.${_PATCH_VERSION})
-    get_cpack_filename(${VERSION} PROJECT_CPACK_FILENAME)
-    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${VERSION}\n${VERSION_DATETIME}\n${PROJECT_CPACK_FILENAME}")
-
+    write_version(${VERSION_FILE} ${_MAJOR_VERSION} ${_MINOR_VERSION} ${_PATCH_VERSION})
 endfunction(check_version)
-
-function(report_version name ver)
-
-    string(ASCII 27 Esc)
-    set(BoldYellow  "${Esc}[1;33m")
-    set(ColourReset "${Esc}[m")
-
-    message("${BoldYellow}${name} version ${ver}${ColourReset}")
-
-endfunction()
-
-# macro to find packages on the host OS
-macro( find_exthost_package )
-    if(CMAKE_CROSSCOMPILING)
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
-
-        find_package( ${ARGN} )
-
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-    else()
-        find_package( ${ARGN} )
-    endif()
-endmacro()
-
-
-# macro to find programs on the host OS
-macro( find_exthost_program )
-    if(CMAKE_CROSSCOMPILING)
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
-
-        find_program( ${ARGN} )
-
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-    else()
-        find_program( ${ARGN} )
-    endif()
-endmacro()
-
-
-function(get_prefix prefix IS_STATIC)
-  if(IS_STATIC)
-    set(STATIC_PREFIX "static-")
-      if(ANDROID)
-        set(STATIC_PREFIX "${STATIC_PREFIX}android-${ANDROID_ABI}-")
-      elseif(IOS)
-        set(STATIC_PREFIX "${STATIC_PREFIX}ios-${IOS_ARCH}-")
-      endif()
-    endif()
-  set(${prefix} ${STATIC_PREFIX} PARENT_SCOPE)
-endfunction()
-
-
-function(get_cpack_filename ver name)
-    get_compiler_version(COMPILER)
-    
-    if(NOT DEFINED BUILD_STATIC_LIBS)
-      set(BUILD_STATIC_LIBS OFF)
-    endif()
-
-    get_prefix(STATIC_PREFIX ${BUILD_STATIC_LIBS})
-
-    set(${name} ${PROJECT_NAME}-${ver}-${STATIC_PREFIX}${COMPILER} PARENT_SCOPE)
-endfunction()
-
-function(get_compiler_version ver)
-    ## Limit compiler version to 2 or 1 digits
-    string(REPLACE "." ";" VERSION_LIST ${CMAKE_C_COMPILER_VERSION})
-    list(LENGTH VERSION_LIST VERSION_LIST_LEN)
-    if(VERSION_LIST_LEN GREATER 2 OR VERSION_LIST_LEN EQUAL 2)
-        list(GET VERSION_LIST 0 COMPILER_VERSION_MAJOR)
-        list(GET VERSION_LIST 1 COMPILER_VERSION_MINOR)
-        set(COMPILER ${CMAKE_C_COMPILER_ID}-${COMPILER_VERSION_MAJOR}.${COMPILER_VERSION_MINOR})
-    else()
-        set(COMPILER ${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION})
-    endif()
-
-    if(WIN32)
-        if(CMAKE_CL_64)
-            set(COMPILER "${COMPILER}-64bit")
-        endif()
-    endif()
-
-    set(${ver} ${COMPILER} PARENT_SCOPE)
-endfunction()
